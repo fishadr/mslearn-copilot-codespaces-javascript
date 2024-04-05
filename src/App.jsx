3cd242dbde6f5208076246a71dc0ce75816bf7cf
fishadr <33906407+fishadr@users.noreply.github.com>
2024-04-05 10:57:42 +0000

    Updated and personalised

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Adrian Fish",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "fishadr@hotmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "fishadr",
   medium: "",
-  twitter: "microsoft",
+  twitter: "fishadr",
   youTube: "Code",
 };
 
